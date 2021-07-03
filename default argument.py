6f864c30b414067f3e2e7f10b7753b9de5b84ac2
KaviyaGopi <kaviyagopi002@gmail.com>
2021-07-03 11:33:57 +0530

    First commit

---
+++ default argument.py
@@ -0,0 +1,5 @@
+#default argument takes the default value defined in the function defenition if there is no arguments passed in the funcyion call
+def my_fun(name="raghul"):
+     print(f"{name} has got the first marks in the class")
+#If i have not pass a parameter in function call it will print the default value in the function defenition     
+my_fun("srinivs")
